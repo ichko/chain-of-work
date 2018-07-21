4081268fa8d277828471a4072dcb74222fa23e56
Iliya Zhechev <i4ko95@gmail.com>
2018-07-21 17:44:31 +0300

    Simple mining example

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,19 @@
-import ikoin.Block
+import koin.BlockChain
+import koin.Miner
+import java.util.*
 
 fun main(args: Array<String>) {
+    val blockChain = BlockChain(difficulty = 5)
+    val miner = Miner(blockChain)
 
-    val genesisBlock = Block(previousHash = "0", data = "I'm the first")
-    val secondBlock = Block(genesisBlock.hash, "I'm the second")
-    val thirdBlock = Block(secondBlock.hash, "I'm the third")
+    (0..8).forEach {
+        val randomData = UUID.randomUUID().toString()
+        val minedBlock = miner.mine(randomData)
 
-    println(genesisBlock)
-    println(secondBlock)
-    println(thirdBlock)
+        if (blockChain.add(minedBlock)) {
+            println("$it mined block: $minedBlock")
+        } else {
+            print("Block was not mined!")
+        }
+    }
 }
