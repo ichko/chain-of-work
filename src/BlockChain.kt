9023653b9e8c76bdebb544128df94e5cce904369
Iliya Zhechev <i4ko95@gmail.com>
2018-07-21 17:44:14 +0300

    Implement simple block chain DS

--- src/BlockChain.kt
+++ src/BlockChain.kt
@@ -1,9 +1,21 @@
-package ikoin
+package koin
 
-class BlockChain {
-    private var blocks = mutableListOf<Block>()
+class BlockChain(difficulty: Int) {
+    private val genesisBlock = Block("no-previous-hash", "no-data")
+    private val blocks = mutableListOf(genesisBlock)
+    val validPrefix = "0".repeat(difficulty)
 
-    fun add(block: Block) {
-        blocks.add(block)
+    private fun isValid(block: Block) = block.hash().startsWith(validPrefix) &&
+        block.previousHash == blocks.last().hash() &&
+        (1..blocks.lastIndex).all { blocks[it].previousHash == blocks[it - 1].hash() }
+
+    fun add(block: Block) = when {
+        (!isValid(block)) -> false
+        else -> {
+            blocks.add(block)
+            true
+        }
     }
+
+    fun last() = blocks.last()
 }
